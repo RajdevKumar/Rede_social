393dc204b8c6319177fe49619bbef25913b366aa
RajdevKumar <rajdev_kumar@hotmail.com>
2019-04-21 15:57:20 +0100

    URl funcionando

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+
+    context = {
+        "title":"Pagina-Inicial"
+    }
+
+    return render(request,'index.html',context)
