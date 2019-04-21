1115a93a68f7ce0df41669a955c79b1ff50fdd4f
RajdevKumar <rajdev_kumar@hotmail.com>
2019-04-21 22:14:45 +0100

    Last commit of the day

--- app/views.py
+++ app/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from app.models import Profile
 
 # Create your views here.
 
 def index(request):
 
     context = {
-        "title":"Pagina-Inicial"
+        "title":"Pagina-Inicial",
+        "user": User.objects.get(username=request.user.username)
     }
 
     if not request.user.is_authenticated:
 
         return render(request,'index.html',context)
     else:
+        context["title"] = "logged";
+    
+
         return render(request,'logged_index.html',context)
