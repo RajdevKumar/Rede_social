2d3ed11302be497cb03a1e5e9b44b388dd3dfcb5
RajdevKumar <rajdev_kumar@hotmail.com>
2019-04-21 20:29:41 +0100

    Extend model working in shell

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 def fnc(instance,name):
@@ -12,3 +14,13 @@ class Profile(models.Model):
     Avatar = models.FileField(upload_to=fnc)
     N_seguidores = models.IntegerField(default=0,null=False)
     N_seguindo = models.IntegerField(default=0,null=False)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
