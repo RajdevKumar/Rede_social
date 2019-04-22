9f28633bba55db724063d054135c1ff56239b541
RajdevKumar <rajdev_kumar@hotmail.com>
2019-04-22 21:29:05 +0100

    Debug off

--- rede_social/settings.py
+++ rede_social/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6$yhn^n^%!qpd)l@8_+abi0oetb%jl+55tzj@jyq^_@b@5706w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
